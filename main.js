ed6476769cbd896f51ce30cd3eb66379b7f84be5
Ariq Daffa <ariqdaffaathallah@gmail.com>
2021-03-03 13:18:41 +0700

    added var vs const

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log("Hello ES6")
+// ES5
+// var myName = "Ariq";
+// myName = "Sinarmas";
+// myName = "Rudi";
+
+// console.log(myName);
+
+// ES6
+// const / constant
+const myCar = "Mazda RX-7"
+// myCar = "Agya"
+// myCar = "Ferrari"
+
+console.log(myCar)
